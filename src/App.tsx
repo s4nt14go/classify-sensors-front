612c5560e9080794eba1e183742ba511e3092e30
s4nt14go <santiagopergamo@gmail.com>
2021-05-03 09:47:55 -0300

    feat: add check for results
    Add library syntax highlighter

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import {uploadViaPresignedPost} from "./lib";
 import config from "./config";
 import Notification from "./components/Notification";
 import {ulid} from "ulid";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import styles from "./App.module.css";
 
 function App() {
 
@@ -14,7 +16,9 @@ function App() {
   const [ sessionId, setSessionId ] = useState<any>();
 
   useEffect(()=>{
-    setSessionId(ulid());
+    const id = ulid();
+    console.log(`sessionId: ${id}`);
+    setSessionId(id);
   }, []);
 
   async function upload() {
@@ -22,6 +26,20 @@ function App() {
     const result = await uploadViaPresignedPost(config.API_URL, file, sessionId);
     console.log('result', result);
     NotificationRef.current.start(result.msg, result.severity);
+  }
+
+  const [ results, setResults ] = useState<any>(null);
+  async function checkResults() {
+    console.log('checkResults');
+    let response = await fetch(config.API_URL + 'check-results?' +  new URLSearchParams({
+      sessionId,
+    }));
+    let json = await response.json();
+    console.log('results', json);
+    for (const i of json.Items) {
+      if (i.classification) i.classification = JSON.parse(i.classification);
+    }
+    setResults(json.Items);
   }
 
   return (
@@ -33,9 +51,25 @@ function App() {
         <Dropzone setFile={setFile} />
 
         <button onClick={upload}
-          className="mb-3 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          className="mb-10 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg block m-auto">
           Upload
         </button>
+
+        <div className='text-left'>
+          <button onClick={checkResults}
+                  className="mb-3 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Check for results!
+          </button>
+
+          {
+            results &&
+            <div className='mb-5'>
+              <SyntaxHighlighter language='json' className={`${styles.json}`}>
+                {JSON.stringify(results, null, 2)}
+              </SyntaxHighlighter>
+            </div>
+          }
+        </div>
 
       </div>
 
